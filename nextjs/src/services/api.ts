e5ba32dbb21b556cfa7813ea236fd318c654d37f
Luan Messias <luanmessias10@gmail.com>
2021-01-18 11:12:52 -0300

    Project instalation

---
+++ nextjs/src/services/api.ts
@@ -0,0 +1,12 @@
+import axios from 'axios'
+
+const api = axios.create({
+  // Local
+  baseURL: 'http://localhost:3333'
+
+  // Online
+  // baseURL: 'https://api.mocki.io/v1/b995b517'
+  // baseURL: 'https://aogia64qlf.api.quickmocker.com/'
+})
+
+export default api
