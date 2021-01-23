e10f98bebf0595999875014db8078f715fb9e7cd
Luan Messias <luanmessias10@gmail.com>
2021-01-23 17:17:58 -0300

    Fixed charset bug in Head component

--- nextjs/src/components/organisms/HeadApp/index.tsx
+++ nextjs/src/components/organisms/HeadApp/index.tsx
@@ -4,7 +4,7 @@ import Head from 'next/head'
 const HeadApp: React.FC = () => {
   return (
     <Head>
-      <meta charset="utf-8" />
+      <meta charSet="utf-8" />
       <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
       <meta
         name="viewport"
