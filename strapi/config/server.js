f04ddb418e2a26f4d8caabf8af3fc6a2fece1680
Luan Messias <luanmessias10@gmail.com>
2021-01-26 09:11:17 -0300

    Changed project structure

--- strapi/config/server.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', 'ff3dbf42df94025ab55ae5bf951f0536'),
-    },
-  },
-});
