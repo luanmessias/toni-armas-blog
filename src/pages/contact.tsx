f2e92274cced7054a73ce1767aa593dfaa0cd0f4
Luan Messias <luanmessias10@gmail.com>
2021-01-31 01:55:53 -0300

    Fix build erros

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,23 +1,20 @@
 import React from 'react'
-import { NotionRenderer } from "react-notion";
+import { NotionRenderer } from 'react-notion'
 
-import "react-notion/src/styles.css";
-import "prismjs/themes/prism-tomorrow.css";
+import 'react-notion/src/styles.css'
+import 'prismjs/themes/prism-tomorrow.css'
 
 export async function getStaticProps() {
   const data = await fetch(
-    "https://notion-api.splitbee.io/v1/table/56e15fc6a74f4ca7b44bae84601996f9"
-  ).then(res => res.json());
-
+    'https://notion-api.splitbee.io/v1/table/56e15fc6a74f4ca7b44bae84601996f9'
+  ).then(res => res.json())
 
   return {
     props: {
       blockMap: data
     }
-  };
+  }
 }
-
-
 const Contact = (blockMap): JSX.Element => {
   console.log(blockMap)
   return (
--- src/pages/quem-somos.tsx
+++ src/pages/quem-somos.tsx
@@ -1,26 +1,21 @@
 import React from 'react'
-import "react-notion/src/styles.css";
-import "prismjs/themes/prism-tomorrow.css";
+import 'react-notion/src/styles.css'
+import 'prismjs/themes/prism-tomorrow.css'
 
-import { NotionRenderer } from "react-notion";
+import { NotionRenderer } from 'react-notion'
 
 export async function getStaticProps() {
   const data = await fetch(
-    "https://notion-api.splitbee.io/v1/page/4247d23bf42242449c0c3d972893d7c1"
-  ).then(res => res.json());
-
-
+    'https://notion-api.splitbee.io/v1/page/4247d23bf42242449c0c3d972893d7c1'
+  ).then(res => res.json())
 
   return {
     props: {
       blockMap: data
     }
-  };
+  }
 }
-
-
-const QuemSomos = ({blockMap}): JSX.Element => {
-  {console.log(blockMap)}
+const QuemSomos = ({ blockMap }): JSX.Element => {
   return (
     <>
       <NotionRenderer blockMap={blockMap} />
@@ -29,8 +24,3 @@ const QuemSomos = ({blockMap}): JSX.Element => {
 }
 
 export default QuemSomos
-
-
-
-
-
