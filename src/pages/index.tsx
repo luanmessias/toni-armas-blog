c08f328aa075bb2bcc4f30ec76e9fc99e3ecbfc2
Luan Messias <luanmessias10@gmail.com>
2021-02-07 14:33:45 -0300

    Add pagination

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PosCard from '@molecules/PostCard'
-import { Container } from '@pageStyles/index'
+import { Container, PaginateContainer } from '@pageStyles/index'
 import { useGetPostListContext } from '@context/GetPostList'
-import { PostPropTypes } from '@proptypes/PostPropTypes'
+import ReactPaginate from 'react-paginate'
 
 const Home = () => {
   const { postList } = useGetPostListContext()
+  const [pageNumber, setPageNumber] = useState(0)
 
-  const postListRender = postList
+  const postsPerPage = 12
+  const pagesVisted = pageNumber * postsPerPage
+  const pageCount = Math.ceil(postList.length / postsPerPage)
+  const changePage = ({ selected }) => {
+    setPageNumber(selected)
+  }
+
+  const displayPosts = postList
+    .slice(pagesVisted, pagesVisted + postsPerPage)
     .filter(({ status }: any) => status === 'ativo')
     .map(({ id, data, titulo, descricao, foto, video }: any, index) => {
       return (
@@ -22,7 +31,30 @@ const Home = () => {
         />
       )
     })
-  return <Container>{postListRender}</Container>
+
+  return (
+    <>
+      <Container>
+        {displayPosts}
+
+        <PaginateContainer>
+          <ReactPaginate
+            previousLabel={'<'}
+            nextLabel={'>'}
+            pageCount={pageCount}
+            onPageChange={changePage}
+            containerClassName={'paginate'}
+            previousLinkClassName={'pagPrev'}
+            nextLinkClassName={'pagNext'}
+            disabledClassName={'pagDisabled'}
+            activeClassName={'pagActive'}
+            pageRangeDisplayed={2}
+            marginPagesDisplayed={0}
+          />
+        </PaginateContainer>
+      </Container>
+    </>
+  )
 }
 
 export default Home
--- src/styles/pages/index.ts
+++ src/styles/pages/index.ts
@@ -1,6 +1,5 @@
 import styled from 'styled-components'
 import { maxDevice } from '@globalStyles/device'
-
 export const Container = styled.div`
   display: flex;
   flex-wrap: wrap;
@@ -10,5 +9,56 @@ export const Container = styled.div`
 
   @media ${maxDevice.laptop} {
     padding-bottom: 100px;
+  }
+`
+
+export const PaginateContainer = styled.div`
+  width: 100%;
+  padding: 15px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  .paginate {
+    font-family: 'Titillium Web', sans-serif;
+    font-size: 18px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: row;
+    list-style: none;
+
+    li {
+      background-color: ${({ theme }) => theme.borderColor};
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      width: 40px;
+      height: 40px;
+      cursor: pointer;
+      margin: 0px 5px;
+      border-radius: 5px;
+
+      &:hover:not(.pagDisabled) {
+        background-color: ${({ theme }) => theme.colorOrangeDark};
+        transform: scale(1.1);
+      }
+
+      &.pagDisabled {
+        opacity: 0.3;
+        cursor: not-allowed;
+      }
+
+      &.pagActive {
+        background-color: ${({ theme }) => theme.colorOrangeMedium};
+      }
+
+      a:active,
+      a:focus {
+        outline: 0;
+        border: none;
+        -moz-outline-style: none;
+      }
+    }
   }
 `
